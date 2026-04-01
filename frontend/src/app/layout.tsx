009531ea6522603609823efc24646055990b2f9a
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-04-01 22:33:30 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (   srs
+  return (   sr
     <html lang="en" data-scroll-behavior="smooth">
       <body suppressHydrationWarning={true}>
         {children}
