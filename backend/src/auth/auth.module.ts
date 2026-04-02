9de678dbfa25d7a92f84f325494ffaa8352a053b
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-04-02 10:42:30 +0530

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { ConfigModule, ConfigService } from '@nestjs/config'; fd
+import { ConfigModule, ConfigService } from '@nestjs/config'; f
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MfaService } from './mfa.service';
