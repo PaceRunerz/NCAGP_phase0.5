df7152c2820a15bfd677e599d46a7492cc6a33fc
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-04-02 10:40:23 +0530

    Update seed.ts

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -9,7 +9,7 @@
 import { PrismaClient, OrgType, UserRole, AssetType, Severity, AuditStatus, FrameworkType } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient();  dfzff
 
 async function main() {
   console.log('🌱 Seeding NCAGP database...\n');
