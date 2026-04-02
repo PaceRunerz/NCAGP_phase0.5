9fec1ebd3cfdfcb5cea44bee4336089491fb3a26
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-04-03 01:22:47 +0530

    Delete frontend/src directory

--- frontend/src/app/dashboard/page.tsx
+++
@@ -1,2 +0,0 @@
-import { NationalRiskHeatmap } from '@/components/dashboard/NationalRiskHeatmap';
-export default function DashboardPage() { return <NationalRiskHeatmap />; }
--- frontend/src/components/FeedbackWidgetClient.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic';
-const FeedbackWidget = dynamic(
-  () => import('./FeedbackWidget').then(m => ({ default: m.FeedbackWidget })),
-  { ssr: false }
-);
-export default function FeedbackWidgetClient() {
-  return <FeedbackWidget />;
-}
