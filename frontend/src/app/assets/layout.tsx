5fbd5109be308b3fb60f5d1e1f38b36eb453efff
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-04-05 14:46:37 +0530

    Update layout.tsx

--- frontend/src/app/assets/layout.tsx
+++ frontend/src/app/assets/layout.tsx
@@ -1,4 +1,4 @@
 import AppShell from '@/components/AppShell';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <AppShell>{children}</AppShell>;
-} khgv
+} khg
