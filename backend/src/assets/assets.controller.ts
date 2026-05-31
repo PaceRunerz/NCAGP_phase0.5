61dfba9685a94f258618191d3369116d54fc6cd7
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-05-31 20:01:51 +0530

    Update assets.controller.ts

--- backend/src/assets/assets.controller.ts
+++ backend/src/assets/assets.controller.ts
@@ -1,5 +1,5 @@
 import {
-  Controller, Get, Post, Param, Body, Query, UseGuards,  eraijgv
+  Controller, Get, Post, Param, Body, Query, UseGuards,  eraijg
 } from '@nestjs/common';
 import { AssetsService } from './assets.service';
 import { JwtAuthGuard, RolesGuard, OrgScopeGuard } from '../auth/guards/auth.guard';
