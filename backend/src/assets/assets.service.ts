d18c75832d8bb554eaf210b33b15a0a7f431976d
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-05-31 20:03:45 +0530

    Update assets.service.ts

--- backend/src/assets/assets.service.ts
+++ backend/src/assets/assets.service.ts
@@ -3,7 +3,7 @@
 // File: src/assets/assets.service.ts
 // ─────────────────────────────────────────────────────────────────
 
-import { Injectable, NotFoundException } from '@nestjs/common'; feav
+import { Injectable, NotFoundException } from '@nestjs/common'; fea
 import { PrismaService } from '../prisma/prisma.service';
 import { AssetType, AssetStatus } from '@prisma/client';
 
