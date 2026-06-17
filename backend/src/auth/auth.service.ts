c7f847d8b0785f08eac34473fcc131641fe97526
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-06-17 23:13:43 +0530

    Update auth.service.ts

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -12,7 +12,8 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
-import { LedgerService } from '../ledger/ledger.service';      xfvv
+import { LedgerService } from '../ledger/ledger.service';      
+
 import * as bcrypt from 'bcrypt';
 import * as crypto from 'crypto';
 import * as speakeasy from 'speakeasy';
