c5116c44a48b57c151b8b25a6404b2ce0fde1d30
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-06-17 23:13:03 +0530

    Update assets.module.ts

--- backend/src/assets/assets.module.ts
+++ backend/src/assets/assets.module.ts
@@ -5,7 +5,7 @@
 // assets.module.ts
 import { Module } from '@nestjs/common'; 
 import { AssetsService } from './assets.service';
-import { AssetsController } from './assets.controller';   df
+import { AssetsController } from './assets.controller';   d
 
 @Module({
   providers: [AssetsService],
