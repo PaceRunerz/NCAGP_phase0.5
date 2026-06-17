75855a24e161f38cd42888e892e1016c3cbbaee9
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-06-17 23:12:45 +0530

    Update assets.module.ts

--- backend/src/assets/assets.module.ts
+++ backend/src/assets/assets.module.ts
@@ -5,7 +5,7 @@
 // assets.module.ts
 import { Module } from '@nestjs/common'; 
 import { AssetsService } from './assets.service';
-import { AssetsController } from './assets.controller';   dfvgv
+import { AssetsController } from './assets.controller';   dfvg
 
 @Module({
   providers: [AssetsService],
