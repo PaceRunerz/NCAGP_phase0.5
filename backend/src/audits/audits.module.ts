eb5548247b804d70b7cb9ab91e2c28da0c9355a9
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-06-19 13:19:58 +0530

    Update audits.module.ts

--- backend/src/audits/audits.module.ts
+++ backend/src/audits/audits.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuditsController } from './audits.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { LedgerModule } from '../ledger/ledger.module';
-
+ gbbg
 @Module({
   imports: [PrismaModule, LedgerModule],
   controllers: [AuditsController],
