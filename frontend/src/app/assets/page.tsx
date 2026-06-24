813d486d1503101ae9c7a19530dc486f2a23264c
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-06-24 22:37:32 +0530

    Update page.tsx

--- frontend/src/app/assets/page.tsx
+++ frontend/src/app/assets/page.tsx
@@ -2,7 +2,7 @@
 import { Suspense } from 'react';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { Server, AlertTriangle, RefreshCw, Shield } from 'lucide-react';   jus tof rdemo 
+import { Server, AlertTriangle, RefreshCw, Shield } from 'lucide-react';   jus tof 
 import { useRouter } from 'next/navigation';
 
 
