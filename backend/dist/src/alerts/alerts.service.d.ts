5e08e36594b7ffb89124925d71077f3f184c8236
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-06-25 00:24:32 +0530

    Update alerts.service.d.ts

--- backend/dist/src/alerts/alerts.service.d.ts
+++ backend/dist/src/alerts/alerts.service.d.ts
@@ -1,7 +1,7 @@
 import { OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../prisma/prisma.service';
-export declare class AlertsService implements OnModuleInit {    s
+export declare class AlertsService implements OnModuleInit {    
     private prisma;
     private config;
     private readonly logger;
