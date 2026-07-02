32998cde689cd62e3ae9615f4e7ce4391393ab1c
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-07-02 20:00:14 +0530

    Update evidence.module.ts

--- backend/src/evidence/evidence.module.ts
+++ backend/src/evidence/evidence.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EvidenceService } from './evidence.service';
 import { EvidenceController } from './evidence.controller';
-import { LedgerModule } from '../ledger/ledger.module';      dfvre
+import { LedgerModule } from '../ledger/ledger.module';      dfvr
 import { PrismaModule } from '../prisma/prisma.module';
 import { StorageModule } from '../storage/storage.module';
 
