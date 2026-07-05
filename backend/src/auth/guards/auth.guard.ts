9eefb45432a30d104836b38b5b17fde118173807
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-07-05 23:19:58 +0530

    Update auth.guard.ts

--- backend/src/auth/guards/auth.guard.ts
+++ backend/src/auth/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import {
   Injectable, ExecutionContext,
-  UnauthorizedException, ForbiddenException, CanActivate, d
+  UnauthorizedException, ForbiddenException, CanActivate, 
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
