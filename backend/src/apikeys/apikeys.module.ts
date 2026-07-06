a60e565d8c5da08f8fd6731b4d505d746040bd33
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-07-06 21:52:51 +0530

    Update apikeys.module.ts

--- backend/src/apikeys/apikeys.module.ts
+++ backend/src/apikeys/apikeys.module.ts
@@ -1,7 +1,7 @@
 // apikeys.module.ts
 import { Module } from '@nestjs/common';
 import { ApiKeysService } from './apikeys.service';
-import { ApiKeysController } from './apikeys.controller';   s
+import { ApiKeysController } from './apikeys.controller';   
 import { LedgerModule } from '../ledger/ledger.module'; 
  
 @Module({
