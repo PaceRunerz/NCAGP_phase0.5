231fa0eb4afc3cbf39d1a80920dacb54dfb8e15c
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-07-10 21:09:52 +0530

    Update audits.module.ts

--- backend/src/audits/audits.module.ts
+++ backend/src/audits/audits.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuditsController } from './audits.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { LedgerModule } from '../ledger/ledger.module';
+import { LedgerModule } from '../ledger/ledger.module';   fvdveve
  
 @Module({
   imports: [PrismaModule, LedgerModule],
