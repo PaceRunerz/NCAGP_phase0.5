5d3758fdf2f965b27f4fe2133057ba1756f7edae
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-07-10 21:11:29 +0530

    Update audits.controller.ts

--- backend/src/audits/audits.controller.ts
+++ backend/src/audits/audits.controller.ts
@@ -4,7 +4,7 @@ import { LedgerService } from '../ledger/ledger.service';
 import { JwtAuthGuard, RolesGuard } from '../auth/guards/auth.guard';
 import { Roles, CurrentUser } from '../auth/roles.decorator';
 import { IsString, IsDateString, IsOptional, IsArray, IsEnum } from 'class-validator';
-import { AuditStatus, FrameworkType } from '@prisma/client';  sdv
+import { AuditStatus, FrameworkType } from '@prisma/client';  sd
 
 class CreateAuditDto {
   @IsString()    orgId: string;
