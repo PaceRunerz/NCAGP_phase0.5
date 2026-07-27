13b899e6453803a862c919eab94e7be3204202fc
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-07-27 22:00:48 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',      db
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',      d
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
