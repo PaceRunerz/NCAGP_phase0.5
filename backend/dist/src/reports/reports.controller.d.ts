444ffd0a05874b4a4f06904e8fd4775df9a279b0
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-09-16 10:15:41 +0530

    Delete backend/dist directory

--- backend/dist/src/reports/reports.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'express';
-import { ReportsService } from './reports.service';
-export declare class ReportsController {
-    private reports;
-    constructor(reports: ReportsService);
-    downloadNationalRisk(res: Response): Promise<void>;
-}
--- backend/dist/src/reports/reports.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-export declare class ReportsService {
-    private prisma;
-    private readonly logger;
-    constructor(prisma: PrismaService);
-    generateNationalRiskReport(): Promise<Buffer>;
-    private buildPDF;
-}
